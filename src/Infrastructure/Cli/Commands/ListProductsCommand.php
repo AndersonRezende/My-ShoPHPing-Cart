a6bf6db79c171dd0358526e89c61c9ad2ca89f68
Anderson Rezende <andersonrezende17@hotmail.com>
2026-01-20 22:25:06 -0300

    Introduce command to list products

---
+++ src/Infrastructure/Cli/Commands/ListProductsCommand.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace MyShoppingCart\Infrastructure\Cli\Commands;
+
+use MyShoppingCart\Application\UseCase\ListProductsUseCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ListProductsCommand extends Command {
+
+    public function __construct(private ListProductsUseCase $listProductsUseCase) {
+        parent::__construct('msp:list-products');
+    }
+
+    protected function configure(): void {
+        $this->setDescription('Lista todos os produtos disponíveis.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Produtos Disponíveis');
+
+        $products = $this->listProductsUseCase->execute();
+
+        $rows = array_map(fn($product) => [
+            $product->id(),
+            $product->name(),
+        ], $products);
+
+        $io->table(['ID', 'Nome'], $rows);
+
+        return Command::SUCCESS;
+    }
+}
---
+++ tests/Infrastructure/Cli/Commands/ListProductsCommandTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace MyShoppingCart\Tests\Infrastructure\Cli\Commands;
+
+use MyShoppingCart\Application\UseCase\ListProductsUseCase;
+use MyShoppingCart\Domain\Entity\Product;
+use MyShoppingCart\Infrastructure\Cli\Commands\ListProductsCommand;
+use MyShoppingCart\Tests\Infrastructure\Persistence\Pdo\DatabaseTestCase;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Attribute\Option;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class ListProductsCommandTest extends TestCase {
+
+    public function testExecuteListProductsCommand(): void {
+        $listProductsUseCase = $this->createMock(ListProductsUseCase::class);
+        $listProductsUseCase->expects($this->once())
+            ->method('execute')
+            ->willReturn(array(
+                new Product('1', 'Product 1'),
+                new Product('2', 'Product 2')
+            )
+        );
+        $listProductsCommand = new ListProductsCommand($listProductsUseCase);
+        $application = new Application();
+        $application->addCommand($listProductsCommand);
+        $command = $application->find('msp:list-products');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+        ]);
+
+        $output = $commandTester->getDisplay();
+
+        $this->assertStringContainsString('Product 1', $output);
+        $this->assertStringContainsString('1', $output);
+        $this->assertStringContainsString('Product 2', $output);
+        $this->assertStringContainsString('2', $output);
+    }
+}
