2b20cb5fe24afdd7d0019df62f7054ec1a6da9de
Anderson Rezende <andersonrezende17@hotmail.com>
2026-01-31 20:49:39 -0300

    Create Checkout cart use case test

---
+++ tests/Application/UseCase/Cart/CheckoutCartUseCaseTest.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace Application\UseCase\Cart;
+
+use LogicException;
+use MyShoppingCart\Application\DTO\CheckoutInput;
+use MyShoppingCart\Application\DTO\ShowCartInput;
+use MyShoppingCart\Application\Repository\CartRepository;
+use MyShoppingCart\Application\UseCase\Cart\CheckoutCartUseCase;
+use MyShoppingCart\Application\UseCase\Cart\ShowCartUseCase;
+use MyShoppingCart\Domain\Entity\Cart;
+use MyShoppingCart\Domain\Entity\Cart\CartBuilder;
+use MyShoppingCart\Domain\Entity\CartItem;
+use MyShoppingCart\Domain\Entity\Product;
+use MyShoppingCart\Domain\Enum\CartStatus;
+use MyShoppingCart\Domain\ValueObject\Money;
+use PHPUnit\Framework\TestCase;
+
+class CheckoutCartUseCaseTest extends TestCase {
+
+    public function testExecuteShouldThrownLogicExceptionWhenCartIsNotOpen(): void {
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Only opened carts can be completed');
+
+        $cartRepository = $this->createMock(CartRepository::class);
+        $cartRepository->expects($this->once())
+            ->method('findById')
+            ->with('nonexistent-cart-id')
+            ->willReturn(new CartBuilder()
+                ->withId('nonexistent-cart-id')
+                ->withStatus(CartStatus::COMPLETED)
+                ->build());
+        $checkoutCartUseCase = new CheckoutCartUseCase($cartRepository);
+
+        $checkoutCartUseCase->execute(new CheckoutInput('nonexistent-cart-id'));
+    }
+
+    public function testExecuteShouldCheckoutCartSuccessfully(): void {
+        $cartRepository = $this->createMock(CartRepository::class);
+        $cartRepository->expects($this->once())
+            ->method('findById')
+            ->with('existing-cart-id')
+            ->willReturn(
+                new CartBuilder()
+                ->withId('existing-cart-id')
+                ->withStatus(CartStatus::OPENED)
+                ->build()
+            );
+        $cartRepository->expects($this->once())
+            ->method('save')
+            ->willReturn(
+                new CartBuilder()
+                ->withId('existing-cart-id')
+                ->withStatus(CartStatus::COMPLETED)
+                ->build()
+            );
+        $checkoutCartUseCase = new CheckoutCartUseCase($cartRepository);
+
+        $cart = $checkoutCartUseCase->execute(new CheckoutInput('existing-cart-id'));
+        
+
+        $this->assertEquals(CartStatus::COMPLETED, $cart->status());
+    }
+}
