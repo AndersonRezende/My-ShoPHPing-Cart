a6adf9751450acf50f21f33eba4db93e3ff816ec
Anderson Rezende <andersonrezende17@hotmail.com>
2026-02-08 23:24:58 -0300

    Introduce to Show cart controller test cart items

--- src/Infrastructure/Http/Controller/Cart/ShowCartController.php
+++ src/Infrastructure/Http/Controller/Cart/ShowCartController.php
@@ -10,9 +10,9 @@ use MyShoppingCart\Domain\Entity\CartItem;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class ShowCartController {
+readonly class ShowCartController {
 
-    public function __construct(private readonly ShowCartUseCase $showCartUseCase) {}
+    public function __construct(private ShowCartUseCase $showCartUseCase) {}
 
     public function __invoke(Request $request, Response $response, array $args): Response {
         $showCartInput = new ShowCartInput($args['id']);
@@ -31,14 +31,13 @@ class ShowCartController {
     }
 
     private function formatPayload(Cart $cart): array {
-        $payload = [
+        return [
             'cart' => [
                 'id' => $cart->id(),
                 'status' => $cart->status(),
                 'items' => $this->formatItems($cart)
             ]
         ];
-        return $payload;
     }
 
     private function formatItems(Cart $cart): array {
--- tests/Infrastructure/Http/Controller/Cart/ShowCartControllerTest.php
+++ tests/Infrastructure/Http/Controller/Cart/ShowCartControllerTest.php
@@ -5,7 +5,10 @@ namespace MyShoppingCart\Tests\Infrastructure\Http\Controller\Cart;
 use InvalidArgumentException;
 use MyShoppingCart\Application\UseCase\Cart\ShowCartUseCase;
 use MyShoppingCart\Domain\Entity\Cart;
+use MyShoppingCart\Domain\Entity\CartItem;
+use MyShoppingCart\Domain\Entity\Product;
 use MyShoppingCart\Domain\Enum\CartStatus;
+use MyShoppingCart\Domain\ValueObject\Money;
 use MyShoppingCart\Infrastructure\Http\Controller\Cart\ShowCartController;
 use PHPUnit\Framework\Attributes\AllowMockObjectsWithoutExpectations;
 use PHPUnit\Framework\TestCase;
@@ -31,7 +34,8 @@ class ShowCartControllerTest extends TestCase {
         $cart = $this->createMock(Cart::class);
         $cart->method('id')->willReturn('valid-cart-id');
         $cart->method('status')->willReturn(CartStatus::OPENED);
-        $cart->method('items')->willReturn([]);
+        $cartItem = new CartItem('1', new Product('1', 'Product-1'), 1, new Money(100));
+        $cart->method('items')->willReturn([$cartItem]);
         $useCase = $this->createMock(ShowCartUseCase::class);
         $useCase->method('execute')->willReturn($cart);
         $controller = new ShowCartController($useCase);
@@ -45,11 +49,17 @@ class ShowCartControllerTest extends TestCase {
             'cart' => [
                 'id' => 'valid-cart-id',
                 'status' => CartStatus::OPENED,
-                'items' => []
-                ]
+                'items' => [[
+                    'productId' => $cartItem->product()->id(),
+                    'name' => $cartItem->product()->name(),
+                    'quantity' => $cartItem->quantity(),
+                    'price' => $cartItem->unitPrice()->amount(),
+                    'subTotal' => $cartItem->subTotal()->amount()
+                ]]
+            ]
         ]);
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals($expectedPayload, $bodyContent);
-    }    
+    }
 }
