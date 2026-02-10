d88e98974ad5fd546bf6befbeac30e7a5967ce78
Anderson Rezende <andersonrezende17@hotmail.com>
2026-02-09 22:00:04 -0300

    Fix create users table

--- database/migrations/20260109000000_create_users_table.php
+++ database/migrations/20260109000000_create_users_table.php
@@ -6,12 +6,12 @@ final class CreateUsersTable extends AbstractMigration
 {
     public function change(): void
     {
-        $table = $this->table('users', ['id' => false, 'primary_key' => 'id']);
-        $table->addColumn('id', 'string', ['limit' => 36])
-              ->addColumn('name', 'string', ['limit' => 255])
-              ->addColumn('email', 'string', ['limit' => 255])
-              ->addColumn('password', 'string', ['limit' => 255])
-              ->addIndex(['email'], ['unique' => true])
-              ->create();
+        $this->table('users')
+            ->addColumn('name', 'string', ['limit' => 255])
+            ->addColumn('email', 'string', ['limit' => 255])
+            ->addColumn('password', 'string', ['limit' => 255])
+            ->addTimestamps()
+            ->addIndex(['email'], ['unique' => true])
+            ->create();
     }
 }
